f56916046350b1a5f9402bd886563c917bd28fde
plrs9816 <61955474+plrs9816@users.noreply.github.com>
2023-01-09 03:03:11 +0900

    feat: initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1312px",
+    },
+    colors: {
+      red: {
+        DEFAULT: "#ff4040",
+        500: "#ff9f9f",
+      },
+      black: "#212121",
+      purple: "#4040ff",
+      gray: {
+        800: "#666666",
+        600: "#999999",
+        400: "#bfbfbf",
+        200: "#d9d9d9",
+        100: "#eeeeee",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
