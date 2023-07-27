5c7089d0834b71886b4a9c9ffec7cf0f1d02bb8c
plrs9816 <61955474+plrs9816@users.noreply.github.com>
2023-07-28 08:04:03 +0900

    feat: Google Analytics integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
-import { API } from "@lib/utils/consts";
+import { isDevEnv } from "@lib/utils/env";
 
 export default function Document() {
   return (
@@ -10,6 +11,21 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+      {!isDevEnv && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script
+            id="google-analytics"
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || []; function gtag(){window.dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');`,
+            }}
+          />
+        </>
+      )}
     </Html>
   );
 }
